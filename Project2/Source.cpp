b72c0938ea1a352a648a9d9983bdc41227849870
humoncy <pengod4150@gmail.com>
2017-05-16 00:55:28 +0800

    find good points pairs

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -14,60 +14,20 @@ Mat img_sample, img_target;
 Mat* img_puzzles;
 const int K = 2;
 
+void readImage();
+void doSIFT();
 void kNN(int k, Mat descriptors_1, Mat descriptors_2 , Mat knn_mat);
-void findFeaturepoints(Mat descriptors_1, Mat descriptors_2, Mat knn_mat, Mat good_pairs);
+void findFeaturepoints(Mat descriptors_1, Mat descriptors_2, Mat knn_mat, Mat& good_pairs);
+void drawKeypointsPairs(vector<KeyPoint> keypoints_1, vector<KeyPoint> keypoints_2, Mat good_pairs);
 
-void readImage()
-{
-	int data;
-	string name;
+int main() {
 
-	while (1) {
-		cout << "Which object do you want? (1:table, 2:logo, 3:other)" << endl;
-		cin >> data;
-		if (data == 1) {
-			name = "table";
-			break;
-		}
-		else if (data == 2) {
-			name = "logo";
-			break;
-		}
-		else if (data == 3) {
-			cout << "Input your testing data name: ";
-			cin >> name;
-			break;
-		}
-		else {
-
-		}
-	}
+	readImage();
 	
-	string path_name = "test/" + name + "/";
-	img_sample = imread(path_name + "sample.bmp");
-	img_target = imread(path_name + "target.bmp");
+	doSIFT();
 
-	int num_puzzles = 1;
-	while(1) {
-		fstream fs(path_name + "puzzle" + to_string(num_puzzles) + ".bmp");
-		if (fs) {
-			num_puzzles++;
-		}
-		else {
-			num_puzzles--;
-			break;
-		}
-	}
-	cout << "number of pizzles: " << num_puzzles << endl;
-	img_puzzles = new Mat[num_puzzles];
-	for (int i = 0; i < num_puzzles; i++) {
-		img_puzzles[i] = imread(path_name + "puzzle" + to_string(i + 1) + ".bmp");
-		//imshow("puzzle" + to_string(i + 1), img_puzzles[i]);
-	}
-
-
-	//Mat imgTry = imread(path_name + "puzzle" + to_string(i) + ".bmp");
-	//imshow("try", imgTry);
+	waitKey(0);
+	return 0;
 }
 
 void doSIFT()
@@ -94,28 +54,21 @@ void doSIFT()
 	drawMatches(img_sample, keypoints_1, img_target, keypoints_2, matches, img_matches);
 	imshow("match", img_matches);*/
 
-	Mat feature1;
+	/*Mat feature1;
 	drawKeypoints(img_sample, keypoints_1, feature1, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 	imshow("result1", feature1);
 	Mat feature2;
 	drawKeypoints(img_target, keypoints_2, feature2, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	imshow("result2", feature2);
+	imshow("result2", feature2);*/
 
 	Mat knn_mat(descriptors_1.rows, K, CV_32S, Scalar(0));
 	kNN(K, descriptors_1, descriptors_2, knn_mat);
 
 	Mat good_pairs;
 	findFeaturepoints(descriptors_1, descriptors_2, knn_mat, good_pairs);
-}
+	//cout << good_pairs;
+	drawKeypointsPairs(keypoints_1, keypoints_2, good_pairs);
 
-int main() {
-
-	readImage();
-	
-	doSIFT();
-
-	waitKey(0);
-	return 0;
 }
 
 void kNN(int k, Mat descriptors_1, Mat descriptors_2, Mat knn_mat)
@@ -137,7 +90,7 @@ void kNN(int k, Mat descriptors_1, Mat descriptors_2, Mat knn_mat)
 	}
 }
 
-void findFeaturepoints(Mat descriptors_1, Mat descriptors_2, Mat knn_mat, Mat good_pairs)
+void findFeaturepoints(Mat descriptors_1, Mat descriptors_2, Mat knn_mat, Mat& good_pairs)
 {
 	int num_good = 0;
 	vector<int> good_indices;
@@ -156,5 +109,69 @@ void findFeaturepoints(Mat descriptors_1, Mat descriptors_2, Mat knn_mat, Mat go
 		good_pairs.at<int>(i, 0) = good_indices.at(i);
 		good_pairs.at<int>(i, 1) = knn_mat.at<int>(good_indices.at(i), 0);
 	}
-	cout << good_pairs;
+	//cout << good_pairs;
+}
+
+void drawKeypointsPairs(vector<KeyPoint> keypoints_1, vector<KeyPoint> keypoints_2, Mat good_pairs)
+{
+	vector<KeyPoint> pair1, pair2;
+	for (int i = 0; i < good_pairs.rows; i++) {
+		pair1.push_back(keypoints_1.at(good_pairs.at<int>(i, 0)));
+		pair2.push_back(keypoints_2.at(good_pairs.at<int>(i, 1)));
+	}
+	Mat pair_feature1;
+	drawKeypoints(img_sample, pair1, pair_feature1, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	imshow("result1", pair_feature1);
+	Mat pair_feature2;
+	drawKeypoints(img_target, pair2, pair_feature2, Scalar(255, 255, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	imshow("result2", pair_feature2);
+}
+
+void readImage()
+{
+	int data;
+	string name;
+
+	while (1) {
+		cout << "Which object do you want? (1:table, 2:logo, 3:other)" << endl;
+		cin >> data;
+		if (data == 1) {
+			name = "table";
+			break;
+		}
+		else if (data == 2) {
+			name = "logo";
+			break;
+		}
+		else if (data == 3) {
+			cout << "Input your testing data name: ";
+			cin >> name;
+			break;
+		}
+		else {
+
+		}
+	}
+
+	string path_name = "test/" + name + "/";
+	img_sample = imread(path_name + "sample.bmp");
+	img_target = imread(path_name + "target.bmp");
+
+	int num_puzzles = 1;
+	while (1) {
+		fstream fs(path_name + "puzzle" + to_string(num_puzzles) + ".bmp");
+		if (fs) {
+			num_puzzles++;
+		}
+		else {
+			num_puzzles--;
+			break;
+		}
+	}
+	cout << "number of pizzles: " << num_puzzles << endl;
+	img_puzzles = new Mat[num_puzzles];
+	for (int i = 0; i < num_puzzles; i++) {
+		img_puzzles[i] = imread(path_name + "puzzle" + to_string(i + 1) + ".bmp");
+		//imshow("puzzle" + to_string(i + 1), img_puzzles[i]);
+	}
 }
